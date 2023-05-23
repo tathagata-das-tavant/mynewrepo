f96f15f40515d241ad52b17ab59fd824a9ef1a6d
Tathagata Das(Tatha) <tathagata.das@tavant.com>
2023-05-23 17:36:54 +0530

    err01 and err02 merged and commited

--- func.sql
+++ func.sql
@@ -1,6 +1,6 @@
 select * from DW.DimCustomer
-Where Cust_id=01 and cust_type = 'B'
+Where Cust_id=01 and cust_type = 'C'
 
 update DW.DimCustomer
 set cust_ph = null
-where cust_type = 'B'
+where cust_type = 'C'
